c4bba8640476a44193d16ae560a9ac0f9f5b8335
Kamil Sikora <kamilsikoora@gmail.com>
2022-12-09 18:03:38 +0100

    Added new svg icons

--- src/app/shared/components/svg-icon/enums/svg-icon-name.enum.ts
+++ src/app/shared/components/svg-icon/enums/svg-icon-name.enum.ts
@@ -1,3 +1,9 @@
 export enum SvgIconName {
   ArrowBack = 'arrow-back',
+  ArrowMore = 'arrow-more',
+  FlagPoland = 'flag-poland',
+  FlagUSA = 'flag-usa',
+  Location = 'location',
+  Logo = 'logo',
+  Time = 'time',
 }
